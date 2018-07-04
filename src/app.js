a62f226551cec8e879d051a68f2df40e39b72735
Sampsa Saari <sampsa.saari@gmail.com>
2018-07-04 09:40:35 +0300

    React basics.

---
+++ src/app.js
@@ -0,0 +1,39 @@
+console.log('App.js is running!');
+
+const app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: []
+};
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+
+  const option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    console.log(app.options);
+  }
+}
+
+const template = (
+  <div>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+    <form onSubmit={onFormSubmit}>
+      <input type="text" name="option" />
+      <button>Add option</button>
+    </form>
+  </div>
+);
+
+
+const appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot)
